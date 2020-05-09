ad46e62c22892a7eabe88087c3f9f6b58dc4af58
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-05-09 19:24:43 +0100

    Add delete all button so that entries can be removed

--- js/app.js
+++ js/app.js
@@ -25,10 +25,12 @@ const createConservationListItem = function (form) {
     const conservationListItem  = document.createElement('li'); // Make new list item
     conservationListItem.classList.add('conservation-list-item'); // Add class to h2
 
+    // Add species to new list item common name as a h2 title
     const commonName = document.createElement('h2');
     commonName.textContent = form.commonName.value;
     conservationListItem.appendChild(commonName);
 
+    // Add new species to new list item with scientific name as a h3 header
     const scientificName = document.createElement('h3');
     scientificName.textContent = form.scientificName.value;
     conservationListItem.appendChild(scientificName);
